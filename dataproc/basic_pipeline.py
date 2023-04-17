6355a2dcf0500aafa88c2f07376491c8ff661911
ACYProjects <36143706+ACYProjects@users.noreply.github.com>
2023-04-17 11:56:55 -0600

    Create basic_pipeline.py

---
+++ dataproc/basic_pipeline.py
@@ -0,0 +1,26 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.types import IntegerType
+
+spark = SparkSession.builder.appName("ExamplePipeline").getOrCreate()
+data = spark.read.csv("data.csv", header=True, inferSchema=True)
+preprocessed_data = data.select("col1", "col2", "col3").withColumnRenamed("col1", "new_col1")
+
+filtered_data = preprocessed_data.filter(preprocessed_data.col2 > 10)
+grouped_data = preprocessed_data.groupBy("col1").agg({"col2": "mean", "col3": "max"})
+
+def square(x):
+    return x ** 2
+
+square_udf = udf(square, IntegerType())
+
+new_data = preprocessed_data.withColumn("new_col4", square_udf(preprocessed_data.col3))
+other_data = spark.read.csv("other_data.csv", header=True, inferSchema=True)
+joined_data = preprocessed_data.join(other_data, "col1", "left")
+
+filtered_data.write.mode("overwrite").parquet("filtered_data.parquet")
+grouped_data.write.mode("overwrite").parquet("grouped_data.parquet")
+new_data.write.mode("overwrite").parquet("new_data.parquet")
+joined_data.write.mode("overwrite").parquet("joined_data.parquet")
+
+spark.stop()
