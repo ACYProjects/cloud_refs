0052a39d8f28644b80289bc763a77fd6acc46d2e
ACYProjects <36143706+ACYProjects@users.noreply.github.com>
2023-04-17 18:32:56 -0600

    Update deduplication_by_col2.sql

--- bigquery/deduplication_by_col2.sql
+++ bigquery/deduplication_by_col2.sql
@@ -1,19 +1,8 @@
 SELECT *
 
-FROM (
-
-  SELECT
-
-      *,
-
-      ROW_NUMBER()
-
-          OVER (PARTITION BY col1)
-
-          row_number
-
-  FROM Accidents
-
+FROM ( SELECT *,
+      ROW_NUMBER() OVER (PARTITION BY col1) row_number
+      FROM Accidents
 )
 
 WHERE row_number = 1
