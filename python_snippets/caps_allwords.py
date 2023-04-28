a66c8cdff24ebfaa1a7424a2351536debb56de73
ACYProjects <36143706+ACYProjects@users.noreply.github.com>
2023-04-27 19:01:29 -0600

    Create caps_allwords.py

---
+++ python_snippets/caps_allwords.py
@@ -0,0 +1,9 @@
+my_string = "hello world capitalize all words"
+words = my_string.split() 
+capitalized_words = []
+
+for word in words:
+    capitalized_words.append(word.capitalize())  
+
+capitalized_string = " ".join(capitalized_words) 
+print(capitalized_string)
