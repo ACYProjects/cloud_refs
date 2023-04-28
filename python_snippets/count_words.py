f22a065c8bb38ba0e0b79c25c3fcec0b1116b00a
ACYProjects <36143706+ACYProjects@users.noreply.github.com>
2023-04-27 19:53:09 -0600

    Create count_words.py

---
+++ python_snippets/count_words.py
@@ -0,0 +1,11 @@
+string = "Hello world, hello, word word"
+words = string.split()
+counts = {}
+
+for word in words:
+    if word in counts:
+        counts[word] += 1
+    else:
+        counts[word] = 1
+        
+print(counts)
