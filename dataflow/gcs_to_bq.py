7e4a1c8d6f4e1ece7c408e76537db2de03292558
ACYProjects <36143706+ACYProjects@users.noreply.github.com>
2023-05-12 11:54:19 -0600

    Update gcs_to_bq.py

--- dataflow/gcs_to_bq.py
+++ dataflow/gcs_to_bq.py
@@ -12,13 +12,10 @@ options.view_as(StandardOptions).runner = 'DataflowRunner'
 
 # Define the pipeline
 with beam.Pipeline(options=options) as pipeline:
-    # Read data from GCS
     lines = pipeline | 'ReadFromGCS' >> beam.io.ReadFromText('gs://your-bucket/input/*.txt')
 
-    # Transform data
     transformed_lines = lines | 'TransformData' >> beam.Map(lambda line: line.upper())
 
-    # Write data to BigQuery
     transformed_lines | 'WriteToBigQuery' >> beam.io.WriteToBigQuery(
         'your-project:your-dataset.your-table',
         write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
