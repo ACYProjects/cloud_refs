a11847bbd6714d34d8e5bcea51cbdbadbf9b83b4
ACYProjects <36143706+ACYProjects@users.noreply.github.com>
2023-05-12 20:12:04 -0600

    Update pubsub_to_bq.py

--- dataflow/pubsub_to_bq.py
+++ dataflow/pubsub_to_bq.py
@@ -10,24 +10,19 @@ project = 'my-project'
 topic_name = 'my-topic'
 subscription_name = 'my-subscription'
 
-# Set the BigQuery table information
 dataset_id = 'my-dataset'
 table_id = 'my-table'
 table_schema = 'field1:STRING,field2:INTEGER,field3:FLOAT'
 
-# Set the pipeline options
 options = PipelineOptions()
 options.view_as(StandardOptions).runner = 'DataflowRunner'
 
-# Define the pipeline
 with beam.Pipeline(options=options) as pipeline:
 
-    # Read data from Pub/Sub
     messages = (pipeline
                 | 'ReadFromPubSub' >> ReadFromPubSub(subscription=f'projects/{project}/subscriptions/{subscription_name}')
                 | 'DecodeMessage' >> beam.Map(lambda message: message.decode('utf-8')))
 
-    # Parse the input and write to BigQuery
     (messages
      | 'ParseJSON' >> beam.Map(lambda message: eval(message))
      | 'AddTimestamp' >> beam.Map(lambda element: beam.window.TimestampedValue(element, element['timestamp']))
